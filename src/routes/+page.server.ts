d554b32184aa241054ed478c6fbffad5f347906f
Jon Shipman <jon@jonshipman.com>
2023-11-27 17:16:58 -0600

    moved SPREADSHEETID to env

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,9 @@
 import { doFetch } from '$lib/util/fetch.js';
+import { SPREADSHEETID } from '$env/static/private';
 
 export function load({ fetch }) {
 	return {
-		sheet: doFetch(fetch, '/api/spreadsheet/1KGWHdNAw4F4hI2E0MwzpUYKpkeD9UeIzKGb6MarfNH4'),
+		sheet: doFetch(fetch, '/api/spreadsheet/' + SPREADSHEETID),
 		lsa: doFetch(fetch, '/api/lsa')
 	};
 }
