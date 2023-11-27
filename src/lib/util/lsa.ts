c4b8d7daf66058fd60d1de7954733a6b65409ec8
jonshipman <jon@jonshipman.com>
2023-11-27 17:52:53 -0600

    fuck

--- src/lib/util/lsa.ts
+++ src/lib/util/lsa.ts
@@ -1,11 +1,11 @@
-import { LSA_MANAGER_CUSTOMER_ID } from '$lib/config.server';
+import { LSA_MANAGER_CUSTOMER_ID, LSA_CUSTOMER_ID } from '$lib/config.server';
 import { localservices } from '@googleapis/localservices';
 import type { JWT } from 'google-auth-library';
 
 export async function lsa(serviceAccount: JWT) {
 	const googleLocalservices = localservices({ version: 'v1', auth: serviceAccount });
 	const response = await googleLocalservices.detailedLeadReports.search({
-		query: `manager_customer_id:${LSA_MANAGER_CUSTOMER_ID};customer_id:${LSA_MANAGER_CUSTOMER_ID}`,
+		query: `manager_customer_id:${LSA_MANAGER_CUSTOMER_ID};customer_id:${LSA_CUSTOMER_ID}`,
 		pageSize: 5,
 		'endDate.day': 0,
 		'endDate.month': 0,
